3fda9c761687a20cddaa567cce31997bd7422846
Blazelip <filin1994@rambler.ru>
2020-07-06 17:12:16 +0300

    Создает функционал слайдеров на JS

--- js/script.js
+++ js/script.js
@@ -76,3 +76,51 @@ mapLink.addEventListener("click", function (evt) {
 closeMap.addEventListener("click", function () {
     map.classList.remove("show-map");
 });
+
+
+// SLIDER
+
+let slider = document.querySelector(".js-slider");
+let sliderServices = document.querySelector(".js-slider-services");
+
+let SLIDE_ACTIVE_CLASS = "slider-active";
+let BUTTON_ACTIVE_CLASS = "slider-button-current";
+let SLIDE_SERVICE_ACTIVE_CLASS = "active-service";
+let BUTTON_SERVICE_ACTIVE_CLASS = "service-current-button";
+
+/* функция активирующая слайдер, для работы она требует:
+    sliderContainer - контейнер в котором лежат слайды и кнопки
+    slideActiveClass - активный css-класс слайда
+    buttonActiveClass - активный css-класс кнопки
+*/
+function initSlider(sliderContainer, slideActiveClass, buttonActiveClass) {
+	let slides = sliderContainer.querySelectorAll(".js-slider-track li");
+	let buttons = sliderContainer.querySelectorAll(".js-slider-controls button");
+
+	function removeActiveClass(index) {
+		slides[index].classList.remove(slideActiveClass);
+		buttons[index].classList.remove(buttonActiveClass);
+	}
+
+	function addActiveClass(index) {
+		slides[index].classList.add(slideActiveClass);
+		buttons[index].classList.add(buttonActiveClass);
+	}
+
+	function activeSlide(id) {
+		buttons.forEach(function(_, index) {
+			id === index ? addActiveClass(index) : removeActiveClass(index);
+		});
+	}
+
+	/* задаём каждой кнопке обработчик клика */
+	buttons.forEach(function(button, id) {
+		button.addEventListener("click", function() {
+			activeSlide(id)
+		});
+	});
+}
+
+// Инициализируем слайдер, передаём в функцию нужные параметры для работы
+initSlider(slider, SLIDE_ACTIVE_CLASS, BUTTON_ACTIVE_CLASS);
+initSlider(sliderServices, SLIDE_SERVICE_ACTIVE_CLASS, BUTTON_SERVICE_ACTIVE_CLASS);
